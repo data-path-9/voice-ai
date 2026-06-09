ce94b738683301c9ea0dfea2cb67fd7c8dbb42f6
Prashant Srivastav <p_srivastav@outlook.com>
2026-06-09 15:23:49 +0800

    chore(migration): added migration for audio options

--- api/assistant-api/migrations/000026_migrate_custom_stt_transport_neutral_options.down.sql
+++ api/assistant-api/migrations/000026_migrate_custom_stt_transport_neutral_options.down.sql
@@ -1,20 +1,8 @@
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.ws.query_params'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.query_params';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.ws.query_params' WHERE options.key = 'listen.query_params';
 
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.ws.request_rules'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.request_rules';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.ws.request_rules' WHERE options.key = 'listen.request_rules';
 
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.ws.response_rules'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.response_rules';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.ws.response_rules' WHERE options.key = 'listen.response_rules';
--- api/assistant-api/migrations/000026_migrate_custom_stt_transport_neutral_options.up.sql
+++ api/assistant-api/migrations/000026_migrate_custom_stt_transport_neutral_options.up.sql
@@ -1,20 +1,8 @@
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.query_params'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.ws.query_params';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.query_params' WHERE options.key = 'listen.ws.query_params';
 
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.request_rules'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.ws.request_rules';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.request_rules' WHERE options.key = 'listen.ws.request_rules';
 
-UPDATE public.assistant_provider_model_options options
-SET key = 'listen.response_rules'
-FROM public.assistant_provider_models models
-WHERE options.assistant_provider_model_id = models.id
-  AND models.model_provider_name = 'custom-stt'
-  AND options.key = 'listen.ws.response_rules';
+UPDATE public.assistant_deployment_audio_options options
+SET key = 'listen.response_rules' WHERE options.key = 'listen.ws.response_rules';
